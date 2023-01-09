0d6da22f7c9cedc991fd858dba68f4247dfa2b6d
SL-Pirate <isiraherath626@gmail.com>
2023-01-10 00:33:36 +0530

    Primary mechanisms implemented

--- students.marks.entry.py
+++ students.marks.entry.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
 PROGRAM_NAME = "students-Marks-Entry"
-VERSION = '1.0.0'
+VERSION = '0.2.0'
 
 import tkinter as tk
+from tkinter import scrolledtext
 
 s=[]    # Insert subjects names
-n=[]    # Insert marks
+n=[]    # Insert marks NOTE = this is a list of tuples in the from of (subject of type str, marks of that subject in type float)
 u=1    # use to create a document
 y=1 # use to assign numbers to the document
 t=1 # use to filter the correct numbers
@@ -61,47 +62,166 @@ AF.write('{0}) Student Name: {1}\n'.format(y,p))
 AF.close()
 '''
 
-h=int(input('How many subjects do you have? '))
-for i in range(1,h+1):
-    m=input('Enter Subject {0} name: '.format(i))
-    s.append(str(m))
+subjectsBoard = scrolledtext.ScrolledText(root, font=("Liberation Serief", 15))
+
+_Minimum_ = None
+_Maximum_ = None
+_Total_ = None
+_Avarage_ = None
+
+def Maximum():
+    a = n
+    Max=a[0][1]
+    for i in range(1,len(a)):
+        if a[i][1]>Max:
+            Max=a[i][1]
+    global _Maximum_
+    _Maximum_ = Max
+    return Max
+
+def Minimum():
+    a = n
+    Min=a[0][1]                                                                                          
+    for i in range(1,len(a)):
+        if a[i][1]<Min:
+            Min=a[i][1]
+    global _Minimum_
+    _Minimum_ = Min
+    return Min
+
+def Total():
+    a = n
+    tot=a[0][1]
+    for i in range(1,len(a)):
+        tot+=a[i][1]
+    global _Total_
+    _Total_ = tot
+    return tot
+
+def Avarage():
+    a = n
+    avg=0
+    tot=Total()
+    avg=float(tot)/(len(a))
+    global _Avarage_
+    _Avarage_ = avg
+    return avg
+
+MaxLabel = tk.Label(root, text=f"Maximum:\t {_Maximum_}")
+MinLabel = tk.Label(root, text=f"Minimum:\t {_Minimum_}")
+TotalLabel = tk.Label(root, text=f"Total:\t\t {_Total_}")
+AvgLAbel = tk.Label(root, text=f"Avarage:\t\t {_Avarage_}")
+
+def EditSubjects(name, score, win):
+    if name != None and name != "" and score != None:
+        n.append((name, score))
+        subjectsBoard.configure(state='normal')
+        subjectsBoard.insert(tk.INSERT, f"\n{name}\t{score}")
+        subjectsBoard.configure(state='disabled')
+        MaxLabel.configure(text=f"Maximum:\t {Maximum()}")
+        MinLabel.configure(text=f"Minimum:\t {Minimum()}")
+        TotalLabel.configure(text=f"Total:\t\t {Total()}")
+        AvgLAbel.configure(text=f"Avarage:\t\t {Avarage()}")
+        win.destroy()
+
+def AddNewSubject():
+    subjectWin = tk.Toplevel()
+    subjectWin.grab_set()
+    subjectWin.transient(root)
+    subjectNameInLabel = tk.Label(subjectWin, text="Enter subject name", padx=10, pady=10)
+    subjectScoreInLabel = tk.Label(subjectWin, text="Enter marks for that subject", padx=10, pady=10)
+    subjectNameInEntry = tk.Entry(subjectWin)
+    subjectScoreInEntry = tk.Entry(subjectWin)
     
-for i in range(h):
-    while t>0:
-    # Input subjects marks
-        x=input('Enter {0} marks: '.format(s[i]))
+    def getScore():
+        try:
+            score = float(subjectScoreInEntry.get())
+            return score
+        except Exception:
+            return None
     
-        # Check the compatibility of the input
-        if int(x)>=0:
-            if int(x)<=100:
-                t+=1
-                # Assign values to the list 'n'
-                n.append(int(x))
-                break
-            else:
-                print("You can't input more than 100 values")
-                continue
+    confirmBtn = tk.Button(subjectWin, text="add subject", padx=10, pady=10, command=lambda:EditSubjects(subjectNameInEntry.get(), getScore(), subjectWin))
+    backBtn = tk.Button(subjectWin, text="cancel", padx=10, pady=10, command=subjectWin.destroy)
+    
+    subjectNameInLabel.pack()
+    subjectNameInEntry.pack()
+    subjectScoreInLabel.pack()
+    subjectScoreInEntry.pack()
+    confirmBtn.pack()
+    backBtn.pack()
+    
+
+def RmAll():
+    #TODO
+    pass
+
+subjectsNote = tk.Label(root, text="Subjects")
+subjectsNote.place(anchor='c', relx=0.25, y=20)
+
+subjectsBoard.place(x=10, y=40, relheight=0.75, relwidth=0.5)
+subjectsBoard.configure(state='disabled')
+
+addSubjectBtn = tk.Button(root, padx=10, pady=10, text="Add new subject", command=AddNewSubject)
+addSubjectBtn.place(anchor='c', relx=0.125, rely=0.925, width=125, height=40)
+
+rmAllSubjectsBtn = tk.Button(root, padx=10, pady=10, text="clear all subjects", command=lambda:RmAll)
+rmAllSubjectsBtn.place(anchor='c', relx=0.375, rely=0.925, width=125, height=40)
         
-        elif int(x)<0:
-            print("You can't input negative values")
-            continue
-                                                                                                    # How to return to the while condition when input "String"???
-        else:
-            print("You can only input numbers")
-            continue
+def SaveToDisk():
+    #TODO
+    pass
+
+MaxLabel.place(anchor='c', height=30, width=200, relx=0.75, rely=0.1)
+MinLabel.place(anchor='c', height=30, width=200, relx=0.75, rely=0.2)
+TotalLabel.place(anchor='c', height=30, width=200, relx=0.75, rely=0.3)
+AvgLAbel.place(anchor='c', height=30, width=200, relx=0.75, rely=0.4)
+
+saveToDiskBtn = tk.Button(root, text="Save data to disk", command=lambda:SaveToDisk)
+EXITbtn = tk.Button(root, text="Exit", command=exit)
+
+saveToDiskBtn.place(anchor='c', height=40, width=200, relx=0.75, rely=0.65)
+EXITbtn.place(anchor='c', height=40, width=100, relx=0.75, rely=0.85)
+
+# h=int(input('How many subjects do you have? '))
+# for i in range(1,h+1):
+#     m=input('Enter Subject {0} name: '.format(i))
+#     s.append(str(m))
+    
+# for i in range(h):
+#     while t>0:
+#     # Input subjects marks
+#         x=input('Enter {0} marks: '.format(s[i]))
+    
+#         # Check the compatibility of the input
+#         if int(x)>=0:
+#             if int(x)<=100:
+#                 t+=1
+#                 # Assign values to the list 'n'
+#                 n.append(int(x))
+#                 break
+#             else:
+#                 print("You can't input more than 100 values")
+#                 continue
+        
+#         elif int(x)<0:
+#             print("You can't input negative values")
+#             continue
+# # How to return to the while condition when input "String"???
+#         else:
+#             print("You can only input numbers")
+#             continue
 
 
-# Insert the Subject name and marks into document
-AF=open("Marks.txt","a")
-for p in range(h):
-    i=0
-    AF.write('{0}: {1}\n'.format(s[p],n[i]))
-    i+=1
-AF.close()
-
-
+# # Insert the Subject name and marks into document
+# AF=open("Marks.txt","a")
+# for p in range(h):
+#     i=0
+#     AF.write('{0}: {1}\n'.format(s[p],n[i]))
+#     i+=1
+# AF.close()
 
 # Create loop to the infinity
+'''
 while True:
     # Display the options
     print("What do you want? \n1) Max \n2) Min \n3) Total \n4) Avg \n5) Count \n6) New Entry \n7) Finish")
@@ -110,13 +230,7 @@ while True:
 
     # Find Max
     if c==1:
-        def func(a):
-            global Max
-            Max=a[0]
-            for i in range(1,len(a)):
-                if a[i]>Max:
-                    Max=a[i]
-            return('The Max value is {0}\n'.format(Max))
+
         print(func(n))
 
         # Open a text document
@@ -127,13 +241,7 @@ while True:
 
     # Find Min        
     elif(c==2):
-        def func(a):
-            global Min
-            Min=a[0]                                                                                           
-            for i in range(1,len(a)):
-                if a[i]<Min:
-                    Min=a[i]
-            return('The Min value is {0}\n'.format(Min))
+
         print(func(n))
         # Open a text document
         AF=open("Marks.txt","a")
@@ -143,12 +251,7 @@ while True:
 
     # Find Total
     elif(c==3):
-        def func(a):
-            global tot
-            tot=a[0]
-            for i in range(1,len(a)):
-                tot=tot+a[i]
-            return('The Total is {0}\n'.format(tot))
+
         print(func(n))
         # Open a text document
         AF=open("Marks.txt","a")
@@ -158,13 +261,7 @@ while True:
 
     # Find Avg
     elif(c==4):
-        def func(a):
-            global avg
-            avg,tot=0,a[0]
-            for i in range(1,len(a)):
-                tot=tot+a[i]
-                avg=float(tot)/(len(a))
-            return('The Avarage is {0}\n'.format(avg))
+
         print(func(n))
         # Open a text document
         AF=open("Marks.txt","a")
@@ -241,5 +338,6 @@ while True:
                 AF.write('{0}: {1}\n'.format(s[p],n[i]))
                 i+=1
             AF.close()
+'''
 
 tk.mainloop()
