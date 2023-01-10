1b87e6a111ce13da289c1ae04dbe698fcae55637
SL-Pirate <isiraherath626@gmail.com>
2023-01-10 10:41:16 +0530

    Refactoring

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Students Marks Entry
 
+**NOTE**
+
+You need to have python installed for this program to work
+
+Clone this repository
+
+## Instructions for GUI mode:
+
+This program depends on tkinter. So you need to install it.
+
+Usually this can be achieved by running `pip install tkinter` in a command line
+
+but depending on your Operating System this may or may not work
+
+For example sometimes you will need to install `python-tk` or `python-tkinter` or similar package using your package-manager in most linux distributions
+
+after installing tkinter,
+
+cd into the repository using the command-line and execute the `students.marks.entry.py` 
+
+or just simply execute the script `students.marks.entry.py` in python using a graphical user interface if your operating system supports such actions
+
+## Instructions For Legacy-cli mode:
+
 * First insert all the details of the student(name,subject and marks).
 * This program can get,\
 &ensp;* max of the all subjects\
@@ -43,11 +67,3 @@ n --> Never create a new file. But if there isn't any text file called "Marks", 
 --> 5] count of the all subjects\
 --> 6] add a new student and his/her details\
 --> 7] Save all the details inside the "Marks.txt" and terminate program
-
-## dependancies 
-Tkinter
->pip install tk
-
-* Note
-
-on linux you might need to install tkinter on your system via your package manager too
--- students.marks.entry.py
+++ students.marks.entry.py
@@ -1,33 +1,12 @@
 #!/usr/bin/python
 
-PROGRAM_NAME = "students-Marks-Entry"
-VERSION = '0.5.0'
-
 import tkinter as tk
 from tkinter import scrolledtext
 
-#s=[]    # Insert subjects names
+PROGRAM_NAME = "students-Marks-Entry"
+VERSION = '0.5.0'
+
 n=[]    # NOTE = this is a list of tuples in the from of (subject of type str, marks of that subject in type float)
-#u=1    # use to create a document
-#y=1 # use to assign numbers to the document
-#t=1 # use to filter the correct numbers
-
-'''
-# Create document or not
-while u>0:
-    print('Do you want to create a new document to store results? (y/n)')
-    choice=input('')
-    if choice.lower()=='y':
-        AF=open("Marks.txt","w")
-        AF.close()
-        break
-    elif choice.lower()=='n':
-        break
-    else:
-        continue
-    
-'''
-
 
 root = tk.Tk()
 root.title(PROGRAM_NAME + " - " + VERSION)
@@ -55,16 +34,6 @@ def setStudName(name):
 nameInBtn = tk.Button(nameWin, text="Submit", command=lambda:setStudName(nameInEntry.get()))
 nameInBtn.place(anchor="c", relx=0.5, y=120, width=100, height=50)
 nameWin.protocol('WM_DELETE_WINDOW', lambda:exit())
-
-'''
-p=input('Enter Student Name: ')
-
-# Open a text document
-AF=open("Marks.txt","a")
-# Insert the Student name into document
-AF.write('{0}) Student Name: {1}\n'.format(y,p))
-AF.close()
-'''
 
 subjectsBoard = scrolledtext.ScrolledText(root, font=("Liberation Serief", 12))
 
@@ -195,163 +164,5 @@ saveToDiskConfirmation.place(anchor='c', height=40, width=200, relx=0.75, rely=0
 
 saveToDiskBtn.place(anchor='c', height=40, width=200, relx=0.75, rely=0.65)
 EXITbtn.place(anchor='c', height=40, width=100, relx=0.75, rely=0.85)
-
-# h=int(input('How many subjects do you have? '))
-# for i in range(1,h+1):
-#     m=input('Enter Subject {0} name: '.format(i))
-#     s.append(str(m))
-    
-# for i in range(h):
-#     while t>0:
-#     # Input subjects marks
-#         x=input('Enter {0} marks: '.format(s[i]))
-    
-#         # Check the compatibility of the input
-#         if int(x)>=0:
-#             if int(x)<=100:
-#                 t+=1
-#                 # Assign values to the list 'n'
-#                 n.append(int(x))
-#                 break
-#             else:
-#                 print("You can't input more than 100 values")
-#                 continue
-        
-#         elif int(x)<0:
-#             print("You can't input negative values")
-#             continue
-# # How to return to the while condition when input "String"???
-#         else:
-#             print("You can only input numbers")
-#             continue
-
-
-# # Insert the Subject name and marks into document
-# AF=open("Marks.txt","a")
-# for p in range(h):
-#     i=0
-#     AF.write('{0}: {1}\n'.format(s[p],n[i]))
-#     i+=1
-# AF.close()
-
-# Create loop to the infinity
-'''
-while True:
-    # Display the options
-    print("What do you want? \n1) Max \n2) Min \n3) Total \n4) Avg \n5) Count \n6) New Entry \n7) Finish")
-    # input only the numbers of the above options
-    c=int(input("Enter number: "))
-
-    # Find Max
-    if c==1:
-
-        print(func(n))
-
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the max into document
-        AF.write('* Max: {0}\n'.format(Max))
-        AF.close()
-
-    # Find Min        
-    elif(c==2):
-
-        print(func(n))
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the max into document
-        AF.write('* Min: {0}\n'.format(Min))
-        AF.close()
-
-    # Find Total
-    elif(c==3):
-
-        print(func(n))
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the max into document
-        AF.write('* Total: {0}\n'.format(tot))
-        AF.close()
-
-    # Find Avg
-    elif(c==4):
-
-        print(func(n))
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the max into document
-        AF.write('* Avg: {0}\n'.format(avg))
-        AF.close()
-        
-    # Find Count
-    elif(c==5):
-        print(' Subject count is {0}\n'.format(len(n)))
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the max into document
-        AF.write('* Subject count: {0}\n'.format(len(n)))
-        AF.close()
-
-    # Finish the process
-    elif c==7:
-        break
-    
-
-    # Delete the current list
-    elif c==6:
-        del n   # Delete previous 'n'
-        del s   # Delete previous 's'
-        
-        # Create a new list 'n' and 's'
-        s=[]    # Insert subjects names
-        n=[]    # Insert marks
-        t=1
-
-        y=y+1   # use to update numbers in document
-        p=input('\nEnter Student Name: ')
-        
-        # Open a text document
-        AF=open("Marks.txt","a")
-        # Insert the Student name into document
-        AF.write('\n{0}) Student Name: {1}\n'.format(y,p))
-        AF.close()
-        h=int(input('How many subjects do you have? '))
-        
-        for i in range(1,h+1):
-            m=input('Enter Subject {0} name: '.format(i))
-            s.append(str(m))
-            
-        for i in range(h):
-            while t>0:
-                # Input new subjects marks
-                x=input('Enter {0} marks: '.format(s[i]))
-
-                # Check the compatibility of the input
-                if int(x)>=0:
-                    if int(x)<=100:
-                        t+=1
-                        # Assign values to the list 'n'
-                        n.append(int(x))
-                        break
-                    else:
-                        print("You can't input more than 100 values")
-                        continue
-        
-                elif int(x)<0:
-                    print("You can't input negative values")
-                    continue
-                                                                                                    # How to return to the while condition when input "String"
-                else:
-                    print("You can only input numbers")
-                    continue
-
-            # Insert the Subject name and marks into document
-            AF=open("Marks.txt","a")
-            for p in range(h):
-                i=0
-                AF.write('{0}: {1}\n'.format(s[p],n[i]))
-                i+=1
-            AF.close()
-'''
 
 tk.mainloop()
